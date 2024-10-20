bdeaaf903a38453c46ad40c571e429a4622687aa
0xjrx <mmarquard737@gmail.com>
2024-10-20 14:39:50 +0200

    Added block2poly and poly2block arithmetics

--- kauma.py
+++ kauma.py
@@ -1,1 +1,14 @@
+#!/usr/bin/env python3
+
+from tasks.poly import block2poly, poly2block
+
+
+coefficients = [12, 127, 9, 0]
+p2b = poly2block(coefficients)
+block = p2b.p2b()
+print(block)
+b2p = block2poly(block)
+poly=b2p.b2p()
+print(poly)
+
 
---
+++ tasks/poly.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+import base64
+
+class poly2block:
+    def __init__(self, coefficients: list):
+        self.coefficients = coefficients
+    def p2b(self):
+        uint_128 = 0
+        for x in self.coefficients:
+         uint_128 ^= (1 << x)
+        byte_arr = uint_128.to_bytes(16, byteorder='little')
+        b64_enc = base64.b64encode(byte_arr).decode('utf-8')
+        return b64_enc
+class block2poly:
+    def __init__(self,block: str):
+        self.block = block
+    def b2p(self):
+        byte_arr = base64.b64decode(self.block)
+        uint = int.from_bytes(byte_arr, byteorder='little')
+        coefficients = []
+        for i in range (128):
+            if uint & (1<<i):
+                coefficients.append(i)
+        return coefficients
+
+        
+
+
+
+
+           
+
+
+   
