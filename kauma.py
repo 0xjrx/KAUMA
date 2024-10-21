a1f3df39d053071253ed8e36588c6949302ee219
0xjrx <mmarquard737@gmail.com>
2024-10-21 15:20:28 +0200

    Added argument parser

--- kauma.py
+++ kauma.py
@@ -1,6 +1,22 @@
 #!/usr/bin/env python3
 from tasks.parse import parse_json
+from argparse import ArgumentParser
+
+def get_args():
+    parser = ArgumentParser()
+    parser.add_argument('file', action='store', help='Specify json file')
+    return parser.parse_args()
+        
+        
+def main():
+    try:
+        args = get_args()
+        parser = parse_json(args.file)
+        parser.parse()
+    except ValueError as e:
+        print(f"Error: {e}")
+
+
 
 if __name__ == "__main__":
-    parser = parse_json("json.json")  # Replace with your filename
-    parser.parse()
+    main()
