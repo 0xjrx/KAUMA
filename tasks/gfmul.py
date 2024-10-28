0b6fea91f1e09fbfea1f024d413c2259a6220efd
0xjrx <mmarquard737@gmail.com>
2024-10-28 02:39:25 +0100

    Potential fix for gfmul, fixed modular reduction in loop

--- tasks/gfmul.py
+++ tasks/gfmul.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python3
 import base64
-a= "ARIAAAAAAAAAAAAAAAAAgA=="
-b= "AgAAAAAAAAAAAAAAAAAAAA=="
 
 
 def gfmul(element_1: str, element_2: str):
@@ -12,7 +10,7 @@ def gfmul(element_1: str, element_2: str):
     uint2 = int.from_bytes(byte_arr2, byteorder = 'little')
     byte_arr_poly = base64.b64decode(irr_poly)
     poly = int.from_bytes(byte_arr_poly, byteorder = 'little')
-
+    # print(len(byte_arr1))  -> 16
     result = 0
     x = 1 
     if uint2 & 1:
@@ -20,13 +18,18 @@ def gfmul(element_1: str, element_2: str):
     uint2>>=1
     for _ in range((len(byte_arr2)*8)-1):
         uint1<<=1
-        uint1 ^= poly
+        #print(f"uint1 after {x} try: {uint1.bit_length()}")
+        #print(uint1.bit_length())
+        if uint1.bit_length()>=129:
+            uint1 ^= poly
+        #print(uint1.bit_length())
         if uint2 & 1:
-            result ^= uint1    
+            result ^= uint1
+            #print(result.bit_length())
         uint2 >>= 1
-        x+=1  
-        if uint2 == 0:
-            break
-    byte_arr_res = result.to_bytes(17, byteorder = 'little')
-    res = base64.b64encode(byte_arr_res[:-1]).decode('ascii')
+        #print(uint1.bit_length())
+    #print(result.bit_length())
+    byte_arr_res = result.to_bytes(16, byteorder = 'little')
+    res = base64.b64encode(byte_arr_res).decode('ascii')
+    #print(result.bit_length())
     return(res)
